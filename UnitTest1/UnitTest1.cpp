9fc6f0544f6749fa850d7af65ca8cd495dcaf8c6
Yura <urijkisil2007@gmail.com>
2024-10-16 16:19:39 +0300

    f commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,48 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_7_3_rek/lab_7_3_rek.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(TestFindEqualRowsAndCols)
+        {
+            // Setup a 3x3 matrix
+            int n = 3;
+            int** a = new int* [n];
+            for (int i = 0; i < n; i++)
+                a[i] = new int[n];
+
+            // Test case where row 0 == column 0 and row 1 == column 1
+            a[0][0] = 1; a[0][1] = 2; a[0][2] = 3;
+            a[1][0] = 2; a[1][1] = 1; a[1][2] = 4;
+            a[2][0] = 3; a[2][1] = 4; a[2][2] = 3;
+
+            // Redirect cout to a string stream
+            std::ostringstream oss;
+            std::streambuf* originalCout = std::cout.rdbuf(oss.rdbuf());
+
+            // Call the function
+            FindEqualRowsAndCols(a, n, 0);
+
+            // Restore cout
+            std::cout.rdbuf(originalCout);
+
+            // Check the output
+            std::string output = oss.str();
+            Assert::IsTrue(output.find("Row 0 is equal to Column 0") != std::string::npos);
+            Assert::IsTrue(output.find("Row 1 is equal to Column 1") != std::string::npos);
+
+            // Clean up
+            for (int i = 0; i < n; i++)
+                delete[] a[i];
+            delete[] a;
+        }
+
+	};
+}
---
+++ lab_7_3_rek/lab_7_3_rek.cpp
@@ -0,0 +1,104 @@
+﻿// lab_7_3_rek.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void InputRows(int** a, const int rowCount, const int colCount, int rowNo);
+void PrintRows(int** a, const int rowCount, const int colCount, int rowNo);
+void FindEqualRowsAndCols(int** a, const int n, int k);
+int SumRowsWithNegative(int** a, const int n, int i);
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+    int n;
+
+    cout << "Enter size of matrix (n x n): ";
+    cin >> n;
+
+
+    int** a = new int* [n];
+    for (int i = 0; i < n; i++)
+        a[i] = new int[n];
+
+
+    InputRows(a, n, n, 0);
+    cout << endl;
+    PrintRows(a, n, n, 0);
+    cout << endl;
+
+
+    FindEqualRowsAndCols(a, n, 0);
+    cout << endl;
+
+
+    int sum = SumRowsWithNegative(a, n, 0);
+    cout << "Sum of rows containing at least one negative element: " << sum << endl;
+
+
+    for (int i = 0; i < n; i++)
+        delete[] a[i];
+    delete[] a;
+
+    return 0;
+}
+
+void InputRows(int** a, const int rowCount, const int colCount, int rowNo) {
+    if (rowNo < rowCount) {
+        for (int colNo = 0; colNo < colCount; colNo++) {
+            cout << "a[" << rowNo << "][" << colNo << "] = ";
+            cin >> a[rowNo][colNo];
+        }
+        cout << endl;
+        InputRows(a, rowCount, colCount, rowNo + 1);
+    }
+}
+
+void PrintRows(int** a, const int rowCount, const int colCount, int rowNo) {
+    if (rowNo < rowCount) {
+        for (int colNo = 0; colNo < colCount; colNo++) {
+            cout << setw(4) << a[rowNo][colNo];
+        }
+        cout << endl;
+        PrintRows(a, rowCount, colCount, rowNo + 1);
+    }
+}
+
+void FindEqualRowsAndCols(int** a, int n, int k) {
+    if (k < n) {
+        bool equal = true;
+        for (int j = 0; j < n; j++) {
+            if (a[k][j] != a[j][k]) {
+                equal = false;
+                break;
+            }
+        }
+        if (equal) {
+            cout << "Row " << k << " is equal to Column " << k << endl;
+        }
+        FindEqualRowsAndCols(a, n, k + 1);
+    }
+}
+
+int SumRowsWithNegative(int** a, const int n, int i) {
+    if (i < n) {
+        int sum = 0;
+        bool hasNegative = false;
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] < 0) {
+                hasNegative = true;
+                break;
+            }
+        }
+        if (hasNegative) {
+            for (int j = 0; j < n; j++) {
+                sum += a[i][j];
+            }
+        }
+        return sum + SumRowsWithNegative(a, n, i + 1);
+    }
+    return 0;
+}
